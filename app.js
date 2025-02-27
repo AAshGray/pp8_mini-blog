a3953f3fba35cae232bfa9e6da19bfb9b4863041
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-02-27 14:35:58 -0800

    added /submit handler

--- app.js
+++ app.js
@@ -14,6 +14,18 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
+app.post('/submit'), (req, res) => {
+    const newPost = {
+        author: req.body.author,
+        title: req.body.title,
+        content: req.body.content
+    }
+
+    console.log(newPost);
+
+    res.render('confirmation', { post: newPost });
+};
+
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${3000}`);
 })
