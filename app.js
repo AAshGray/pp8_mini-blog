c4c5cef351f0337d67d75611e91fdf9b19d4dfb6
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-02-27 14:22:38 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${3000}`);
+})
