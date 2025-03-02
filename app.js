98dad139acb73cfa11cf660d045bdd6494c87287
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-03-02 11:28:29 -0800

    changes to styles

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('home');
-})
+    res.render('home')
+});
 
-app.post('/submit'), (req, res) => {
+app.post('/submit', (req, res) => {
     const newPost = {
         author: req.body.author,
         title: req.body.title,
@@ -24,7 +24,7 @@ app.post('/submit'), (req, res) => {
     console.log(newPost);
 
     res.render('confirmation', { post: newPost });
-};
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${3000}`);
