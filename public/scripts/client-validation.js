4b99e20594a1c9f7ad8edbc5e2b7576c475e78cc
Alex Borovets <Borovets.Alex@student.greenriver.edu>
2025-03-04 14:57:21 -0800

    added author validation

--- public/scripts/client-validation.js
+++ public/scripts/client-validation.js
@@ -2,6 +2,15 @@ document.getElementsById('post-form').onsubmit = validate;
 
 function validate() {
     clearErrors();
+
+    let isValid = true;
+
+    let author = document.getElementById('author').value.trim();
+    if (author === ""){
+        document.getElementById("err-author").style.display = "block";
+        isValid = false;
+    }
+    return isValid;
 }
 
 function clearErrors() {
